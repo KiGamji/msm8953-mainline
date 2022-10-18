f8813826999de1214ce7d42fb5154a897b09eecd
Danila Tikhonov <JIaxyga@protonmail.com>
2022-10-18 23:17:12 +0300

    iio: imu: bmi160: add bmi120 support
    BMI120 is an energy-efficient version of BMI160. Despite having a different
    CHIPID value, this variant seems to be fully compatible with the BMI160
    variant.
    
    Signed-off-by: Danila Tikhonov <JIaxyga@protonmail.com>

--- drivers/iio/imu/bmi160/bmi160_core.c
+++ drivers/iio/imu/bmi160/bmi160_core.c
@@ -27,6 +27,7 @@
 
 #define BMI160_REG_CHIP_ID	0x00
 #define BMI160_CHIP_ID_VAL	0xD1
+#define BMI120_CHIP_ID_VAL	0xD3
 
 #define BMI160_REG_PMU_STATUS	0x03
 
@@ -737,9 +738,9 @@ static int bmi160_chip_init(struct bmi160_data *data, bool use_spi)
 		dev_err(dev, "Error reading chip id\n");
 		goto disable_regulator;
 	}
-	if (val != BMI160_CHIP_ID_VAL) {
-		dev_err(dev, "Wrong chip id, got %x expected %x\n",
-			val, BMI160_CHIP_ID_VAL);
+	if (val != BMI160_CHIP_ID_VAL && val != BMI120_CHIP_ID_VAL) {
+		dev_err(dev, "Wrong chip id, got %x expected %x or %x\n",
+			val, BMI160_CHIP_ID_VAL, BMI120_CHIP_ID_VAL);
 		ret = -ENODEV;
 		goto disable_regulator;
 	}
